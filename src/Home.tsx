79caa88d90c7ba1fa285f1ce7f638db566de7a5d
Antonio Hickey <antoniohickey99@gmail.com>
2023-06-05 15:26:03 -0400

    clean up

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,13 +5,6 @@ import { Pricing } from './components/Pricing'
 export default function Home() {
   return (
     <>
-      <head>
-        <title>Project Omni - A Better Search Engine.</title>
-        <meta
-          name="description"
-          content="Organize the worlds information while guiding user's to signal and away from noise in a very loud world."
-        />
-      </head>
       <main>
         <Hero showBetaReq={true}/>
         <Pricing />
