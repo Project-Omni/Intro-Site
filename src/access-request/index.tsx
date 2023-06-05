d0e6be16ed879f73997eeb78f55aa4eae7b3935b
Antonio Hickey <antoniohickey99@gmail.com>
2023-06-05 15:14:56 -0400

    update access request page ui

--- src/access-request/index.tsx
+++ src/access-request/index.tsx
@@ -41,71 +41,77 @@ export default function AccessRequest() {
         />
       </head>
 
+      <main>
 			<Hero showBetaReq={false}/>
+
+      <hr></hr>
 		
-    	<Container className="py-16 text-center">
-    		<div className="space-y-10 divide-y divide-gray-900/10">
-    		    <div className="px-4 sm:px-0">
-    		      <h2 className="text-base font-semibold leading-7 text-gray-900">Request Beta Access</h2>
-    		      <p className="mt-1 text-sm leading-6 text-gray-600">
-    		        This information will be used to determine if you're fit for beta access, no need to list any identifiable information.
-    		      </p>
-    		    </div>
+      <section className="bg-slate-900">
+    	  <Container className="py-16 text-center">
+    	  	<div className="space-y-10 divide-y divide-gray-900/10">
+    	  	    <div className="px-4 sm:px-0">
+    	  	      <h2 className="text-base font-semibold leading-7 text-gray-200">Request Beta Access</h2>
+    	  	      <p className="mt-1 text-sm leading-6 text-gray-400">
+    	  	        This information will be used to determine if you're fit for beta access, no need to list any identifiable information.
+    	  	      </p>
+    	  	    </div>
 
-    		    <div className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
-    		      <div className="px-4 py-6 sm:p-8">
-    		        <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-    		          <div className="col-span-full">
-    		            <label className="block text-sm font-medium leading-6 text-gray-900">
-    		              How do you use your current search engine?
-    		            </label>
-    		            <div className="mt-2">
-    		              <textarea
-    		                id="answer-1"
-    		                name="answer-1"
-												ref={answer1}
-    		                rows={3}
-    		                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-    		                defaultValue={''}
-    		              />
-    		            </div>
-    		          </div>
+    	  	    <div className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl w-1/2 mx-auto">
+    	  	      <div className="px-4 py-6 sm:p-8">
+    	  	        <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+    	  	          <div className="col-span-full">
+    	  	            <label className="block text-sm font-medium leading-6 text-gray-900">
+    	  	              How do you use your current search engine?
+    	  	            </label>
+    	  	            <div className="mt-2">
+    	  	              <textarea
+    	  	                id="answer-1"
+    	  	                name="answer-1"
+			  									ref={answer1}
+    	  	                rows={3}
+    	  	                className="block w-full rounded-md border border-blue-500 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+    	  	                defaultValue={''}
+    	  	              />
+    	  	            </div>
+    	  	          </div>
 
-    		          <div className="col-span-full">
-    		            <label className="block text-sm font-medium leading-6 text-gray-900">
-    		              What do you hope to see in the future of search engines & the web in general?
-    		            </label>
-    		            <div className="mt-2">
-    		              <textarea
-    		                id="answer-2"
-    		                name="answer-2"
-												ref={answer2}
-    		                rows={3}
-    		                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-    		                defaultValue={''}
-    		              />
-    		            </div>
-    		          </div>
+    	  	          <div className="col-span-full">
+    	  	            <label className="block text-sm font-medium leading-6 text-gray-900">
+    	  	              What do you hope to see in the future of search engines & the web in general?
+    	  	            </label>
+    	  	            <div className="mt-2">
+    	  	              <textarea
+    	  	                id="answer-2"
+    	  	                name="answer-2"
+			  									ref={answer2}
+    	  	                rows={3}
+    	  	                className="block w-full rounded-md border border-blue-500 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+    	  	                defaultValue={''}
+    	  	              />
+    	  	            </div>
+    	  	          </div>
 
-    		        </div>
-    		      </div>
-    		      <div className="flex items-center justify-center gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
-    		        <button
-    		          type="submit"
-    		          className="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-								  onClick={async () => {
-										if (answer1.current && answer1.current.value != "" && answer2.current && answer2.current?.value != "") {
-											let sent = await sendAccessRequest(answer1.current?.value!, answer2.current?.value!);
-											if (sent) pageNav('/') 
-										}										
-								  }}
-    		        >
-    		          Send
-    		        </button>
-    		      </div>
-						</div>
-				</div>
-			</Container>
+    	  	        </div>
+    	  	      </div>
+    	  	      <div className="flex items-center justify-center gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
+    	  	        <button
+    	  	          type="submit"
+    	  	          className="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+			  					  onClick={async () => {
+			  							if (answer1.current && answer1.current.value != "" && answer2.current && answer2.current?.value != "") {
+			  								let sent = await sendAccessRequest(answer1.current?.value!, answer2.current?.value!);
+			  								if (sent) pageNav('/') 
+			  							}										
+			  					  }}
+    	  	        >
+    	  	          Send
+    	  	        </button>
+    	  	      </div>
+			  			</div>
+			  	</div>
+			  </Container>
+      </section>
+      </main>
 
     	<Footer/>
 		</div>
